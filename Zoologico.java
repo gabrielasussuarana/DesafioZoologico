37bda0a55d0155571aca06a8d61130221ca60882
gabrielasussuarana <79941146+gabrielasussuarana@users.noreply.github.com>
2022-10-28 17:59:41 -0300

    Update Zoologico.java

--- Zoologico.java
+++ Zoologico.java
@@ -19,11 +19,5 @@ public class Zoologico {
             System.out.println("Jaula " + x + " " + animal.getClass().getCanonicalName() + " " + animal.getNome());
             x++;
         }
-        /*
-        for (int i = 0; i < 9; i++) {
-            System.out.println("Jaula " + i + " " + jaula.get(i).getClass().getCanonicalName());
-        }
-        */
-
     }
 }
